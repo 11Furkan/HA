0e9aa6ab47432ba298d0fa53ee3495d51b36dc3a
Furkan Yaglidere <115979551+11Furkan@users.noreply.github.com>
2023-01-16 17:40:08 +0100

    Add files via upload

---
+++ Furkan_InsertionSort.py
@@ -0,0 +1,14 @@
+def insertion_sort(list_a):
+    indexing_length = range(1, len(list_a))
+    for i in indexing_length:
+        value_to_sort = list_a[i]
+
+        while list_a[i-1] > value_to_sort and i>0:
+            list_a[i], list_a[i-1] = list_a[i-1], list_a[i]
+            i = i-1
+
+    return list_a
+
+
+
+print(insertion_sort([5,2,55,8,7,84,22,15,659,444,55,845,777,]))
